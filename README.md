b5fba4dfb6f5cbd7ec23f9321124d2f61e171be3
Boris Kaul <localvoid@gmail.com>
2015-09-08 11:59:41 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# gcc-bootstrap
+
+Lightweight bootstrap module for
+[google-closure-compiler](https://github.com/google/closure-compiler)
+that works only in compiled mode.
---
+++ bootstrap.js
@@ -0,0 +1,59 @@
+/**
+ * @fileoverview Lightweight bootstrap for the Google Closure
+ * compiler that works in compiled mode only.
+ *
+ * @author arv@google.com (Erik Arvidsson)
+ * @author localvoid@gmail.com (Boris Kaul)
+ *
+ * @provideGoog
+ */
+
+/**
+ * @define {boolean} Overridden to true by the compiler when
+ *     --process_closure_primitives is specified.
+ */
+var COMPILED = false;
+
+/**
+ * Base namespace for the Closure library.  Checks to see goog is already
+ * defined in the current scope before assigning to prevent clobbering if
+ * base.js is loaded more than once.
+ *
+ * @const
+ */
+var goog = goog || {};
+
+/**
+ * Handles strings that are intended to be used as CSS class names.
+ *
+ * This function works in tandem with @see goog.setCssNameMapping.
+ *
+ * Without any mapping set, the arguments are simple joined with a hyphen and
+ * passed through unaltered.
+ *
+ * When there is a mapping, there are two possible styles in which these
+ * mappings are used. In the BY_PART style, each part (i.e. in between hyphens)
+ * of the passed in css name is rewritten according to the map. In the BY_WHOLE
+ * style, the full css name is looked up in the map directly. If a rewrite is
+ * not specified by the map, the compiler will output a warning.
+ *
+ * When the mapping is passed to the compiler, it will replace calls to
+ * goog.getCssName with the strings from the mapping, e.g.
+ *     var x = goog.getCssName('foo');
+ *     var y = goog.getCssName(this.baseClass, 'active');
+ *  becomes:
+ *     var x= 'foo';
+ *     var y = this.baseClass + '-active';
+ *
+ * If one argument is passed it will be processed, if two are passed only the
+ * modifier will be processed, as it is assumed the first argument was generated
+ * as a result of calling goog.getCssName.
+ *
+ * @param {string} className The class name.
+ * @param {string=} opt_modifier A modifier to be appended to the class name.
+ * @return {string} The class name or the concatenation of the class name and
+ *     the modifier.
+ */
+goog.getCssName = function(className, opt_modifier) {
+  return opt_modifier === void 0 ? className + '-' + opt_modifier : className;
+};
